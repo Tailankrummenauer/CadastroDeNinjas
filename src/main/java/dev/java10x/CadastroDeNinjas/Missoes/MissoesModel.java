ae65a41ffe7aee44cb7fc490c878045a5a42277e
Tailan Rodrigues <tailankrummenauer@gmail.com>
2026-04-24 23:22:24 -0300

    modelagem do banco de dados, ninjaModel e missoesModel concluidos, anotations feitos para relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,23 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tabela_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Character rank;
+
+
+    //Uma Missao pode ter muitos ninjas
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+
+}
