9967f7e00c29909594f9e6d370bc99bfee1eb8ef
Tailan Rodrigues <tailankrummenauer@gmail.com>
2026-04-27 15:59:22 -0300

    Feat: Camadas feitas para repository, service, controller

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,6 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 
 import java.util.List;
