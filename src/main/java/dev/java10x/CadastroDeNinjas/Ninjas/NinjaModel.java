97c400c18d38b4a5b5a95bbc32defdd2e32c8179
Tailan Rodrigues <tailankrummenauer@gmail.com>
2026-05-14 13:07:46 -0300

    fix: Tira a responsabilidade do model entity e passa para dentro do DTO, usando stream no mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -29,11 +29,15 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name = "rank")
+    private String rank;
 
     //@ManytoOne um ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missoes_id", nullable = true)//foreing key ou chave estrangeira
     private MissoesModel missoes;
+
+
 
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -11,35 +13,47 @@ public class NinjaService {
 
     //@Autowired ----  fazer isso apenas para injecao de dependencia(fazer o uso de uma classe\interface), mas uma boa pratica é usar o construtor
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     //Listar todos ninjas
-    public List<NinjaModel> listarNinjas(){
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listarNinjas(){
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
     }
 
     //Listar ninjas por ID
-    public NinjaModel listarNinjaId(Long id){
-        return ninjaRepository.findById(id).orElseThrow(() -> new RuntimeException("Ninja não encontrado"));
+    public NinjaDTO listarNinjaId(Long id){
+       Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.map(ninjaMapper::map).orElse(null);
     }
 
     //Adicionar novo ninja
-    public NinjaModel adicionarNinja(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
+    public NinjaDTO adicionarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
+
     //Deletar novo ninja, tem que ser metodo VOID
     public void deletarNinjaId(Long id){
         ninjaRepository.deleteById(id);
     }
 
     //Atualizar novo ninja, passa id como busca e nome como edição
-    public NinjaModel atualizarNomeNinja(Long id, NinjaModel ninja) {
-        if (ninjaRepository.existsById(id)) {
-            ninja.setId(id);
-            return ninjaRepository.save(ninja);
+    public NinjaDTO atualizarNomeNinja(Long id, NinjaDTO ninja) {
+        Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id);
+        if (ninjaExistente.isPresent()) {
+            NinjaModel ninjaAtualizado = ninjaMapper.map(ninja);
+            ninjaAtualizado.setId(id);
+            ninjaAtualizado = ninjaRepository.save(ninjaAtualizado);
+            return ninjaMapper.map(ninjaAtualizado);
         }
         return null;
     }
